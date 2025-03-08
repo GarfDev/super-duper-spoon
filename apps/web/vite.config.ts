f5cc799ec244a18937246491ca56de0340005120
Garfield <gnguyengia@pay.com.au>
2025-03-09 00:30:19 +0700

    feat: enable HTTPS

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,47 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      strategies: "injectManifest",
+      srcDir: "src",
+      filename: "sw.ts",
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: "web",
+        short_name: "web",
+        description: "nothing yet",
+        theme_color: "#ffffff",
+      },
+
+      injectManifest: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+      },
+
+      devOptions: {
+        enabled: true,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+
+  server: {
+    https: {
+      key: "../../configs/certs/key.pem",
+      cert: "../../configs/certs/cert.pem",
+    },
+  },
+});
